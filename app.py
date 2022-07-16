80d56a6757351053763053aaf85aeedeef750823
Drew <108194033+drewpagnotta@users.noreply.github.com>
2022-07-16 03:40:59 -0600

    Updated Comments for Developers to Understand
    Provided # Comments for each function, loop, and code to at least show developers and viewers what I was trying to accomplish.

--- app.py
+++ app.py
@@ -119,25 +119,28 @@ def save_qualifying_loans(qualifying_loans):
 # Given that I’m using the loan qualifier CLI, when I run the qualifier, then the tool should prompt the user to save the results as a CSV file.
     # DONE, LINE 132
 # Given that no qualifying loans exist, when prompting a user to save a file, then the program should notify the user and exit.
-    # DONE, LINE 154-155
+    # DONE, LINE 157-158
 # Given that I have a list of qualifying loans, when I’m prompted to save the results, then I should be able to opt out of saving the file.
-    # DONE, LINE 138-139
+    # DONE, LINE 140-141
 # Given that I have a list of qualifying loans, when I choose to save the loans, the tool should prompt for a file path to save the file.
-    # DONE, LINE 134-136
+    # DONE, LINE 135-137
 # Given that I’m using the loan qualifier CLI, when I choose to save the loans, then the tool should save the results as a CSV file.
-    # DONE, LINE 141-150
+    # DONE, LINE 144-153
 
-
+    # If there's at least one qualifying loan, the user will be asked if they want to save their list.
     if len(qualifying_loans) > 0:
         save = questionary.text("Would you like to save your list of qualifying loans? Type Yes or No.").ask()
 
+        # If the user answers Yes to save, then a CSV file is generated.
         if save == "Yes":
             csvpath = questionary.text("Where would you like your file to be saved? We will generate a file called loan_qualifier_data.csv").ask()
             csvpath = Path(csvpath)
             
+            # If the user declines to save their list of loans, they can review the list without saving.
             if save == "No":
                 print("That's okay! Please review your list of loans at this time.")
             
+            # The user answers Yes to save, and the CSV file generates with the list of headers and appropriate data.
             if save == "Yes":
                 header = ["Lender", "Max Loan Amount", "Max LTV", "Max DTI", "Min Credit Score", "Interest Rate"]
                 csvpath = Path("loan_qualifier_data.csv")
@@ -147,9 +150,9 @@ def save_qualifying_loans(qualifying_loans):
                     csvwriter = csv.writer(csvfile)
                     csvwriter.writerow(header)
                     for loan in loan_qualifier_data:
-                        csvwriter.writerow(loan.values())
+                        csvwriter.writerow(loan)
 
-
+    # If the user doesn't qualify for any loans, they are told nicely and are prompted to exit the application.
     else:
         print("I'm sorry, you have no qualifying loan offers.")
         sys.exit("The process is complete, you may exit the Terminal.")
